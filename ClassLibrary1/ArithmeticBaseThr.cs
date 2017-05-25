a312a00a628884b465ff39e771ba4ca4aedcdbb4
kirigakuretora <fe21st@yahoo.com.tw>
2017-05-25 19:21:09 +0800

    update ArithmeticBaseThr.cs

--- ClassLibrary1/ArithmeticBaseThr.cs
+++ ClassLibrary1/ArithmeticBaseThr.cs
@@ -1,21 +1,72 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ClassLibrary1
 {
-    public sealed class ArithmeticBaseThr
+    public class ArithmeticBaseThr
     {
-        public Int16 Arithmetic(Dictionary<Int16, Int16> First)
+
+        private Int32 Result { get; set; }
+        public ArithmeticBaseThr()
         {
-            return 0;
+            Result = 0;
         }
 
-        public Int32 ArithmeticSecound(Dictionary<Int32,Int32> First)
+        /// <summary>
+        /// Arithmetics the specified first.
+        /// </summary>
+        /// <param name="First">The first.</param>
+        /// <returns></returns>
+        public Int32 Arithmetic(Dictionary<Int32,Int32> First)
+        {          
+            foreach (var Sum in First)
+            {
+                Result+=Sum.Value;
+            }
+            return Result;
+        }
+
+        /// <summary>
+        /// Arithmetics the secound.
+        /// </summary>
+        /// <param name="First">The first.</param>
+        /// <returns></returns>
+        public Int32 ArithmeticSecound(SortedList<Int32, Int32> First)
         {
-            return 0;
+            foreach (var Sum in First)
+            {
+                Result += Sum.Value;
+            }
+            return Result;
+        }
+
+        /// <summary>
+        /// Arithmetics the third.
+        /// </summary>
+        /// <param name="First">The first.</param>
+        /// <returns></returns>
+        public Int32 ArithmeticThird(Int32[] First)
+        {
+            foreach (var Sum in First)
+            {
+                Result += Sum;
+            }
+            return Result;
+        }
+
+        /// <summary>
+        /// Arithmetics the fourth.
+        /// </summary>
+        /// <param name="First">The first.</param>
+        /// <returns></returns>
+        public Int32 ArithmeticFourth(List<Int32>First)
+        {
+            foreach (var Sum in First)
+            {
+                Result += Sum;
+            }
+            return Result;
         }
 
     }
---
+++ WebApplication1.Tests/ArithmeticBaseThrUT.cs
@@ -0,0 +1,93 @@
+﻿using ClassLibrary1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace WebApplication1.Tests
+{
+    [TestClass]
+    public class ArithmeticBaseThrUT
+    {
+        /// <summary>
+        /// Generic Result
+        /// </summary>
+        private Object IsTrue;
+        private void SetIsTrue<T>(T tIsTrue)
+        {
+            IsTrue = tIsTrue;
+        }
+
+        public T getResult<T>()
+        {
+            return (T)IsTrue;
+        }
+
+        /// <summary>
+        /// Tests the dictionary method.
+        /// </summary>
+        [TestMethod()]
+        public void TestDictionaryMethod()
+        {
+            var TestArithmeticBaseThr = new ArithmeticBaseThr();
+            var First = new Dictionary<Int32, Int32>
+            {
+                { 0, 1 },
+                { 1, 2 },
+                { 2, 3 },
+                { 3, 4 },
+                { 4, 5 }
+            };
+            Int32 Result = 15;
+            IsTrue = TestArithmeticBaseThr.Arithmetic(First);
+            Assert.AreEqual(Result, IsTrue);
+        }
+
+
+        /// <summary>
+        /// Tests the dictionary decima method.
+        /// </summary>
+        [TestMethod()]
+        public void TestDictionaryDecimaMethod()
+        {
+            var TestArithmeticBaseThr = new ArithmeticBaseThr();
+            var First = new Dictionary<Int32, Int32>
+            {
+                { 0, -1 },
+                { 1, 2 },
+                { 2, -3 },
+                { 3, 4 },
+                { 4, -5 }
+            };
+            Int32 Result = -3;
+            IsTrue = TestArithmeticBaseThr.Arithmetic(First);
+            Assert.AreEqual(Result, IsTrue);
+
+        }
+
+        /// <summary>
+        /// Tests the arrary list method.
+        /// </summary>
+        [TestMethod()]
+        public void TestArraryListMethod()
+        {
+            var TestArithmeticBaseThr = new ArithmeticBaseThr();
+            var First = new Int32[] { 1, 2, 3, 4, 5 };
+            Int32 Result = 15;
+            IsTrue = TestArithmeticBaseThr.ArithmeticThird(First);
+            Assert.AreEqual(Result, IsTrue);
+        }
+
+        /// <summary>
+        /// Tests the arrary list decima method.
+        /// </summary>
+        [TestMethod()]
+        public void TestArraryListDecimaMethod()
+        {
+            var TestArithmeticBaseThr = new ArithmeticBaseThr();
+            var First = new Int32[] { -1, 2, -3, 4,-5 };
+            Int32 Result = -3;
+            IsTrue = TestArithmeticBaseThr.ArithmeticThird(First);
+            Assert.AreEqual(Result, IsTrue);
+        }
+    }
+}
